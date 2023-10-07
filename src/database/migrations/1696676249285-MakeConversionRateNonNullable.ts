f825ddeb1431d887d7f0096a12c751a6f23d7cc8
Eddie CooRo <eddiecooro@gmail.com>
2023-10-07 14:29:33 +0330

    Make ConversionRate non nullable

---
+++ src/database/migrations/1696676249285-MakeConversionRateNonNullable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class MakeConversionRateNonNullable1696676249285
+  implements MigrationInterface
+{
+  name = 'MakeConversionRateNonNullable1696676249285';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "temporary_currency" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "currencyFrom" varchar NOT NULL, "currencyTo" varchar NOT NULL, "conversionRate" integer NOT NULL, "fromLabel" varchar NOT NULL DEFAULT (''), "toLabel" varchar NOT NULL DEFAULT (''), CONSTRAINT "uniqueExchange" UNIQUE ("currencyFrom", "currencyTo"))`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "temporary_currency"("id", "currencyFrom", "currencyTo", "conversionRate", "fromLabel", "toLabel") SELECT "id", "currencyFrom", "currencyTo", "conversionRate", "fromLabel", "toLabel" FROM "currency"`,
+    );
+    await queryRunner.query(`DROP TABLE "currency"`);
+    await queryRunner.query(
+      `ALTER TABLE "temporary_currency" RENAME TO "currency"`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "currency" RENAME TO "temporary_currency"`,
+    );
+    await queryRunner.query(
+      `CREATE TABLE "currency" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "currencyFrom" varchar NOT NULL, "currencyTo" varchar NOT NULL, "conversionRate" integer, "fromLabel" varchar NOT NULL DEFAULT (''), "toLabel" varchar NOT NULL DEFAULT (''), CONSTRAINT "uniqueExchange" UNIQUE ("currencyFrom", "currencyTo"))`,
+    );
+    await queryRunner.query(
+      `INSERT INTO "currency"("id", "currencyFrom", "currencyTo", "conversionRate", "fromLabel", "toLabel") SELECT "id", "currencyFrom", "currencyTo", "conversionRate", "fromLabel", "toLabel" FROM "temporary_currency"`,
+    );
+    await queryRunner.query(`DROP TABLE "temporary_currency"`);
+  }
+}
